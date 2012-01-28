8f9336c3e08305c14446725bd75c0dc3af82d7ab
Andrey Vityuk <andrey.vityuk@gmail.com>
2012-01-28 12:15:15 -0600

    Added logging library

---
+++ src/main/java/org/jmemcached/Main.java
@@ -0,0 +1,12 @@
+package org.jmemcached;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+}
